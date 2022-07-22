9714802bf9584d8d5151df7f0842a4d20100482f
The Fern <krede2000@gmail.com>
2022-07-22 14:03:59 +0200

    Changed README

---
+++ BSNet/BSNet/Attributes/BSSerializable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using BSNet.Quantization;
+
+namespace BSNet.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class BSSerializableAttribute : Attribute
+    {
+        private BoundedRange range;
+        private Encoding encoding = Encoding.ASCII;
+        
+        public BSSerializableAttribute() {}
+
+        public BSSerializableAttribute(Type encodingType)
+        {
+            this.encoding = (Encoding)Activator.CreateInstance(encodingType);
+        }
+
+        public BSSerializableAttribute(float min, float max, float precision)
+        {
+            range = new BoundedRange(min, max, precision);
+        }
+    }
+}
--- BSNet/Example/ExamplePacket.cs
+++ BSNet/Example/ExamplePacket.cs
@@ -1,12 +1,16 @@
 ﻿using BSNet.Quantization;
 using BSNet.Stream;
 using System.Text;
+using BSNet.Attributes;
 
 namespace BSNet.Example
 {
     public class ExamplePacket : IBSSerializable
     {
+        [BSSerializable(typeof(UTF8Encoding))]
         public string TestString { set; get; }
+        
+        [BSSerializable(-10, 10, 0.00001f)]
         public float TestFloat { set; get; }
 
         private BoundedRange testRange = new BoundedRange(-10, 10, 0.00001f);
